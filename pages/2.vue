ecc51a9815659a457c091498ca9921ddd287d845
LgendZ23 <tadetortosa@gmail.com>
2021-10-28 18:39:08 -0300

    update info pages

--- pages/2.vue
+++ pages/2.vue
@@ -18,7 +18,7 @@
           <v-list-item-content>
             <h3>¿Qué son los DERECHOS?</h3>
             <v-divider class="mb-1"></v-divider>
-            <p>El derecho es un conjunto de principios y normas generalmente inspirados en ideas de justicia y orden, que regulan las relaciones humanas en toda sociedad y cuya observancia es impuesta de forma coactiva por parte de un poder público. El conjunto de principios y normas jurídicas de un Estado determinado constituye su ordenamiento jurídico. El derecho es el objeto de estudio de la ciencia del derecho. El derecho guarda una íntima conexión con la política, la economía, la sociología y la historia, y es el centro de problemas humanos complejos como la determinación de lo que es justo.</p>
+            <p><br>El derecho es un conjunto de principios y normas generalmente inspirados en ideas de justicia y orden, que regulan las relaciones humanas en toda sociedad y cuya observancia es impuesta de forma coactiva por parte de un poder público. El conjunto de principios y normas jurídicas de un Estado determinado constituye su ordenamiento jurídico. El derecho es el objeto de estudio de la ciencia del derecho. El derecho guarda una íntima conexión con la política, la economía, la sociología y la historia, y es el centro de problemas humanos complejos como la determinación de lo que es justo.</p>
           </v-list-item-content>
         </v-list-item>
         <v-list-item three-line>
@@ -26,21 +26,25 @@
             <h3>Derecho objetivo y derecho subjetivo</h3>
             <v-divider class="mb-1"></v-divider>
             <p>El derecho objetivo es el conjunto de normas jurídicas (leyes, reglamentos, entre otras) de carácter obligatorio, y que son creadas por el Estado para conservar el orden social. Siempre teniendo en cuenta la validez, es decir, si se ha llevado a cabo el procedimiento adecuado para su creación, independientemente de su eficacia y de su ideal axiológico (si busca concretar un valor como la justicia, la armonía, el bien común, etcétera).<br><br>El derecho subjetivo es la facultad que ha otorgado el ordenamiento jurídico a un sujeto (por ejemplo, derecho a la nacionalidad, derecho a la salud, derecho a demandar, derecho a manifestarse libre y pacíficamente, derecho a la libre expresión, derecho al sindicalismo, etc.)
-            <br><br>El derecho objetivo puede responder a distintas significaciones:</p>
-            <p class="ma-4">El conjunto de reglas que rigen la convivencia de los hombres en sociedad.</p>
-            <p class="ma-4">Norma o conjunto de normas que por una parte otorgan derechos o facultades y por la otra, correlativamente, establecen o imponen obligaciones.</p>
-            <p class="ma-4">Conjunto de normas que regulan la conducta de los hombres, con el objeto de establecer un ordenamiento justo de convivencia humana. </p>
-            <p>El derecho subjetivo se puede decir que es:</p>
-            <p class="ma-4">La facultad que tiene un sujeto para ejecutar un acto con determinada conducta o abstenerse de ella, o para exigir de otro sujeto el cumplimiento de su deber.
-            <br><br>La facultad, la potestad o autorización que conforme a la norma jurídica tiene un sujeto frente a otro u otros sujetos, ya sea para desarrollar su propia actividad o determinar la de aquellos.
-            <br><br>Es una concesión de autoridad otorgada por una norma jurídica una persona o grupo en virtud de la cual queda legitimada para desplegar una determinada conducta sobre una o más personas o cosas.</p>
+            <br><br><u>El derecho objetivo puede responder a distintas significaciones:</u></p><br>
+            <ul><br>
+            <li>El conjunto de reglas que rigen la convivencia de los hombres en sociedad.</li><br>
+            <li>Norma o conjunto de normas que por una parte otorgan derechos o facultades y por la otra, correlativamente, establecen o imponen obligaciones.</li><br>
+            <li>Conjunto de normas que regulan la conducta de los hombres, con el objeto de establecer un ordenamiento justo de convivencia humana. </li><br>
+            </ul>
+            <p><u>El derecho subjetivo se puede decir que es:</u></p><br>
+            <ul><br>
+            <li>La facultad que tiene un sujeto para ejecutar un acto con determinada conducta o abstenerse de ella, o para exigir de otro sujeto el cumplimiento de su deber.</li><br>
+            <li>La facultad, la potestad o autorización que conforme a la norma jurídica tiene un sujeto frente a otro u otros sujetos, ya sea para desarrollar su propia actividad o determinar la de aquellos.</li><br>
+            <li>Es una concesión de autoridad otorgada por una norma jurídica una persona o grupo en virtud de la cual queda legitimada para desplegar una determinada conducta sobre una o más personas o cosas.</li><br>
+            </ul>
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
           <v-list-item-content>
             <h3>¿Cuál es la diferencia entre los Derechos Humanos y los Derechos Fundamentales?</h3>
             <v-divider class="mb-1"></v-divider>
-            <p>Se diferencian en que los derechos humanos tienen un alcance universal, pues se reconocen a todas las personas en el mundo desde el momento de su nacimiento, los derechos fundamentales tienen un alcance nacional y, dependiendo del país, pueden variar.</p>
+            <p><br>Se diferencian en que los derechos humanos tienen un alcance universal, pues se reconocen a todas las personas en el mundo desde el momento de su nacimiento, los derechos fundamentales tienen un alcance nacional y, dependiendo del país, pueden variar.</p>
           </v-list-item-content>
         </v-list-item>
       </v-card>
--- pages/index.vue
+++ pages/index.vue
@@ -11,14 +11,16 @@
         <v-card :elevation="hover ? 15 : 10" :class="{ 'on-hover':hover }" class="mx-auto mt-3" max-width="100%" tile>
           <v-list-item one-line>
             <v-list-item-content>
-              <h3>¿Qué es el marco jurídico o marco legal?</h3>
+              <h3>¿Qué es el Marco Jurídico o Marco Legal?</h3>
               <v-divider class="mb-1"></v-divider>
-              <p>El marco jurídico es el conjunto de leyes, reglas, legislaciones y cualquier otro instrumento con rango y de carácter legal, que son utilizados de forma secuencial y coherente, los cuales una vez aplicados permiten sustentar todas las actuaciones y actividades en materia legal. Los instrumentos de aspecto legal que son considerados en el marco jurídico están estructurados en diferentes categorías o rangos, estos instrumentos, son de forma general:</p>
-              <p class="ma-4">Los reglamentos y las leyes: en esta categorización de instrumentos se incluyen la constitución de cada país, los códigos aplicables, y las diferentes leyes.</p>
-              <p class="ma-4">Las Resoluciones y los decretos: en esta categoría se incluyen las diferentes resoluciones y decretos que son promulgados y que tienen vigencia en cada uno de los países.</p>
-              <p class="ma-4">Las proidencias de tipo administrativas: las cuales son emanadas de los diferentes órganos o entes con rango y autorización para desarrollar y promulgar es este tipo de instrumentos.</p>
-              <p class="ma-4">Normas de uso generales: en esta categoría se incluyen las diferentes normativas que se derivan de las diferentes leyes o de las providencias de tipo administrativas.</p>
-              <p class="ma-4">Instrumentos de tipo internacional: esta categoría está representada por los diferentes acuerdos o leyes que se rigen entre diversos países, para un mejor entendimiento y manejo de aspectos relacionados en materia legal. </p>
+              <p><br>El Marco Jurídico es el conjunto de leyes, reglas, legislaciones y cualquier otro instrumento con rango y de carácter legal, que son utilizados de forma secuencial y coherente, los cuales una vez aplicados permiten sustentar todas las actuaciones y actividades en materia legal. Los instrumentos de aspecto legal que son considerados en el marco jurídico están estructurados en diferentes categorías o rangos, estos instrumentos, son de forma general:</p>
+              <ul><br>
+                <li>Los reglamentos y las leyes: en esta categorización de instrumentos se incluyen la constitución de cada país, los códigos aplicables, y las diferentes leyes.</li><br>
+                <li>Las Resoluciones y los decretos: en esta categoría se incluyen las diferentes resoluciones y decretos que son promulgados y que tienen vigencia en cada uno de los países.</li><br>
+                <li>Las proidencias de tipo administrativas: las cuales son emanadas de los diferentes órganos o entes con rango y autorización para desarrollar y promulgar es este tipo de instrumentos.</li><br>
+                <li>Normas de uso generales: en esta categoría se incluyen las diferentes normativas que se derivan de las diferentes leyes o de las providencias de tipo administrativas.</li><br>
+                <li>Instrumentos de tipo internacional: esta categoría está representada por los diferentes acuerdos o leyes que se rigen entre diversos países, para un mejor entendimiento y manejo de aspectos relacionados en materia legal.</li><br>
+              </ul>  
               <p>Es de esta forma, que en el marco jurídico se evidencia la existencia de una amplia y diversa gama de leyes e instrumentos aplicados en materia legislativa, en donde dicho ámbito o alcance sirve de uso y aplicación para las diferentes gestiones y regimiento en materia legal, tanto para el gobierno como para los ciudadanos que hacen vida en un determinado país donde se contemplen y apliquen dichas regulaciones. 
               <br><br>Estos instrumentos pueden ser de tipo nacional pero también de tipo internacional, que son aquellos que se establecen entre los diferentes países para regular y unificar la materia legal y que permite el conocimiento de regulaciones entre ambas entidades, por ejemplo las leyes internacionales marítimas, son conocidas y aplicadas a nivel internacional y su contenido incluyendo las regulaciones, alcance y sanciones, son conocidos por todos los entes involucrados, sus términos y disposiciones son usados y aplicados por cada uno de ellos de forma similar.</p>
             </v-list-item-content>
